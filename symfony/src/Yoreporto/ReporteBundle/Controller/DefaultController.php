f8618be06f81bf11133ae1ef435857def085ec71
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-03-05 17:41:17 -0600

    Creación de Formulario de Reporte
    Se creó el formulario, la ruta, y la acción que estará encargado de
    crear un reporte.
    
    Se modificó el modelo de la Base de Datos para poder almacenar
    posteriormente la última fecha en la que el usuario se conectó al
    sistema.

--- symfony/src/Yoreporto/ReporteBundle/Controller/DefaultController.php
+++ symfony/src/Yoreporto/ReporteBundle/Controller/DefaultController.php
@@ -4,10 +4,18 @@ namespace Yoreporto\ReporteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Yoreporto\ReporteBundle\Form\ReportType;
+
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('YoreportoReporteBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function newReportAction(){
+    	$form= $this->createForm(new ReportType());
+
+    	return $this->render("YoreportoReporteBundle:Default:newReport.html.twig", array('form'=> $form->createView()));
     }
 }
